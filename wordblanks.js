314151eaa4db165cba90b3188be1a3764224da4a
Simon Kenny Panigrahi <simonkp.acad@gmail.com>
2025-03-28 20:29:27 +0530

    testin

---
+++ wordblanks.js
@@ -0,0 +1,8 @@
+function wordblanks(Noun, adj, verb, adv){
+    var result = "";
+    result = "a " + adj + " " + Noun + " is " + verb + "ing " + adv 
+
+    return result
+}
+
+console.log(wordblanks("girl","hot","suck","hard"))
